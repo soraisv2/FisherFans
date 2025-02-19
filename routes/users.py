ad5a25dc8d26d51b36301fa624b224ffcbe8db1c
Victor Sorais <victorsorais@mbp-de-victor.speedport.ip>
2025-02-19 17:52:24 +0100

    Authentification by JWT tested with get_user:id route

--- routes/users.py
+++ routes/users.py
@@ -1,29 +1,46 @@
-from flask import Blueprint, request, jsonify, session, redirect, url_for
+from flask import Blueprint, request, jsonify, session, redirect, url_for, current_app
 from app.database import get_db
 from models.users import get_users, add_user, get_user, delete_user, modify_user, login_user
+# JWT secure import
+import jwt, datetime
+from app.utils import token_required
 
 users = Blueprint('users', __name__)
-
-print("test")
-
-@users.route('/login', methods=['POST'])
-def login_route():
-    data = request.get_json()
-    user = login_user(data['email'], data['password'])
-    if user:
-        print(user)
-        session['user_id'] = user[0]
-        session['email'] = user[3]
-        return jsonify({"message": "Users succefuly connected"}), 200
-    else:
-        return jsonify({"message": "Email ou mot de passe incorrect."}), 404
-
 
 @users.route('/add_user', methods=['POST'])
 def add_user_route():
     data = request.get_json()
     add_user(data['lastName'], data['firstName'], data['email'], data['password'], data['boat_license_number'])
     return jsonify({"message": "Utilisateur ajouté avec succès !"})
+
+# @users.route('/login', methods=['POST'])
+# def login_route():
+#     data = request.get_json()
+#     user = login_user(data['email'], data['password'])
+#     if user:
+#         session['user_id'] = user[0]
+#         session['email'] = user[3]
+#         return jsonify({"message": "Users succefuly connected"}), 200
+#     else:
+#         return jsonify({"message": "Email ou mot de passe incorrect."}), 404
+
+@users.route('/login', methods=['POST'])
+def login_route():
+    data = request.get_json()  # Récupère les données envoyées (email, password)
+    user = login_user(data['email'], data['password'])  # Vérifie les identifiants en base
+    if user:
+        token = jwt.encode(
+            {
+                'user_id': user[0],  # ID utilisateur
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1) # Date d'expiration du token
+            },
+            current_app.config['SECRET_KEY'],  # Clé secrète pour signer le token
+            algorithm='HS256'  # Algorithme de chiffrement
+        )
+        return jsonify({"message": "Utilisateur connecté avec succès", "token": token}), 200
+    else:
+        return jsonify({"message": "Email ou mot de passe incorrect."}), 404
+
 
 @users.route('/get_users', methods=['GET'])
 def get_users_route():
@@ -41,6 +58,7 @@ def get_users_route():
     return jsonify(user_list)
 
 @users.route('/get_user', methods=['GET'])
+@token_required
 def get_user_route():
     data = request.get_json()
     user = get_user(data["user_id"])
